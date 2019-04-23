dc411c0e7cedb6ce2a02b23e1ea863a338b419a3
佐藤洸 <satotakeru@team-lab.com>
2019-04-23 12:47:58 +0900

    検索機能完成

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.demo.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,7 @@ import com.example.demo.entiteis.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    @Query(value = "select post from Post post where post.content like %:keyword%")
     List<Post> findByContent(@Param("keyword") String keyword);
+
 }
